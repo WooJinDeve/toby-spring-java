aae0a274c256aee0552a817cc5d96240d0271f74
WoojinDeve <jwj060111@gmail.com>
2023-02-11 17:46:31 +0900

    feat: container less application

---
+++ hello/src/main/java/com/hello/HelloApplication.java
@@ -0,0 +1,47 @@
+package com.hello;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			HelloController helloController = new HelloController();
+
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp)
+						throws ServletException, IOException {
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
+
+						String ret = helloController.hello(name);
+
+						//웹 응답
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					} else if (req.getRequestURI().equals("/user")) {
+						//
+
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+				}
+			}).addMapping("/*");
+		});
+		webServer.start();
+	}
+}
---
+++ hello/src/main/java/com/hello/HelloController.java
@@ -0,0 +1,8 @@
+package com.hello;
+
+public class HelloController {
+
+    public String hello(String name){
+        return "Hello " + name;
+    }
+}
