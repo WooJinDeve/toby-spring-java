864e842b8d54c687aa776632d4cda1933a0970af
WoojinDeve <jwj060111@gmail.com>
2023-02-11 19:42:14 +0900

    feat: bean factory method configuration

--- spring-container/src/main/java/com/springcontainer/SpringContainerApplication.java
+++ spring-container/src/main/java/com/springcontainer/SpringContainerApplication.java
@@ -10,31 +10,48 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class SpringContainerApplication {
+
+    @Bean
+    public HelloController helloController(HelloService helloService){
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService(){
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
         //Spring Container
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh(); // ApplicationContext -> bean create
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
 
-        //Servlet
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(applicationContext)
-                    ).addMapping("/*");
-        });
-        webServer.start();
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext ->
+                        servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)
+                        ).addMapping("/*"));
+                webServer.start();
+            }
+        };
+
+        applicationContext.register(SpringContainerApplication.class);
+        applicationContext.refresh(); // ApplicationContext -> bean create
     }
 }
