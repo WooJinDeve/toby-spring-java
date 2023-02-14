72f6a6153a2d1a240fa7074fe4669c940df584ea
WoojinDeve <jwj060111@gmail.com>
2023-02-14 13:24:56 +0900

    refactor: custom @conditional apply

--- conditional/src/main/java/com/config/autoconfig/JettyWebServerConfig.java
+++ conditional/src/main/java/com/config/autoconfig/JettyWebServerConfig.java
@@ -1,18 +1,13 @@
 package com.config.autoconfig;
 
+import com.config.ConditionalMyOnClass;
 import com.config.MyAutoConfiguration;
-import com.config.autoconfig.JettyWebServerConfig.JettyCondition;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
 
     @Bean("jettyWebServerFactory")
@@ -20,10 +15,4 @@ public class JettyWebServerConfig {
         return new JettyServletWebServerFactory();
     }
 
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
-        }
-    }
 }
