d430634cf30064d6e7fa36b40e26e1016c60e6c2
WoojinDeve <jwj060111@gmail.com>
2023-02-14 12:17:53 +0900

    init: @conditional psubmodule

---
+++ conditional/src/main/java/com/helloboot/ConditionalApplication.java
@@ -0,0 +1,13 @@
+package com.helloboot;
+
+import com.config.MySpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@MySpringBootApplication
+public class ConditionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionalApplication.class, args);
+	}
+
+}
