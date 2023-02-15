d15ab0b2da6563e739f5588d8dacd188839c88bd
WoojinDeve <jwj060111@gmail.com>
2023-02-15 12:43:53 +0900

    feat: dataSource config

---
+++ data-source/src/main/java/com/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,44 @@
+package com.config.autoconfig;
+
+import com.config.ConditionalMyOnClass;
+import com.config.EnableMyConfigurationProperties;
+import com.config.MyAutoConfiguration;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Driver;
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)
+public class DataSourceConfig {
+
+    @Bean
+    @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
+    @ConditionalOnMissingBean
+    public DataSource hikariDataSource(MyDataSourceProperties properties) {
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setDriverClassName(properties.getDriverClassName());
+        dataSource.setJdbcUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+}
