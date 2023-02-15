1caf5a494063c72f60ee50ae9a81b8690e908692
WoojinDeve <jwj060111@gmail.com>
2023-02-15 12:59:28 +0900

    feat: jdbc connection apply

---
+++ data-source/src/test/java/com/annotation/TransactionTest.java
@@ -0,0 +1,23 @@
+package com.annotation;
+
+
+import com.helloboot.DataSourceApplication;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = DataSourceApplication.class)
+@TestPropertySource("classpath:/application.properties")
+@Transactional
+public @interface TransactionTest {
+}
---
+++ data-source/src/test/java/com/helloboot/JdbcTemplateTest.java
@@ -0,0 +1,32 @@
+package com.helloboot;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.annotation.TransactionTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@TransactionTest
+public class JdbcTemplateTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init(){
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void insertAndQuery(){
+        jdbcTemplate.update("INSERT INTO hello VALUES(?, ?)", "woojin", 3);
+        jdbcTemplate.update("INSERT INTO hello VALUES(?, ?)", "spring", 3);
+
+        Long count = jdbcTemplate.queryForObject("SELECT count(*) FROM hello", Long.class);
+
+        assertThat(count).isEqualTo(2);
+    }
+}
